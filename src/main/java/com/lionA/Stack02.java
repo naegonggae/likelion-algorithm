649efec8baa5bb4b3fb80429902e9debd0386060
naegonggae <https://github.com/naegonggae/>
2022-10-20 10:29:55 +0900

    Initial commit

---
+++ src/main/java/com/lionA/Stack02.java
@@ -0,0 +1,42 @@
+package com.lionA;
+
+import java.util.EmptyStackException;
+
+public class Stack02 {
+
+    private Integer[] arr;
+    private int top = 0;
+
+    public Stack02() {
+        this.arr = new Integer[10000];
+    }
+
+    public Stack02(int size) {
+        this.arr = new Integer[size];
+    }
+
+    public Integer[] getArr() {
+        return arr;
+    }
+
+    public void push(int value) {
+        // 10을 넣으면 arr[0] = 10
+        this.arr[this.top++] = value;
+    }
+    public int pop() {
+        if(this.isEmpty()) {  // 비어있는데 pop()하는 경우
+            throw new EmptyStackException();
+        }
+        return this.arr[--this.top];
+    }
+
+    public int peek() {
+        return this.arr[this.top-1];
+    }
+
+    public boolean isEmpty() {
+        return this.top == 0 ? true : false;
+    }
+
+
+}
---
+++ src/test/java/Stack02Test.java
@@ -0,0 +1,66 @@
+import com.lionA.Stack02;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class Stack02Test {
+    Stack02 st = new Stack02();
+
+    @BeforeEach
+    void setUp() {
+        st.push(10);
+        st.push(20);
+        System.out.println("before each");
+    }
+
+    @Test
+    @DisplayName("push가 잘 되는지")
+    void push() {
+        st.push(10);
+        st.push(20);
+        Integer[] arr = st.getArr();
+        assertEquals(20, arr[1]);
+        assertEquals(10, arr[0]);
+    }
+
+    @Test
+    @DisplayName("pop이 잘되는지")
+    void pop() {
+        assertEquals(20, st.pop());
+        assertEquals(10, st.pop());
+
+        // st.pop()이 비어있을땐?
+        // Exception의 검증 (모던 Java 표현)
+        assertThrows(EmptyStackException.class,() -> {
+            st.pop();
+        });
+    }
+
+    @Test
+    @DisplayName("peek 확인")
+    void peek() {
+        assertEquals(20, st.peek());
+    }
+
+    @Test
+    @DisplayName("isEmpty 확인")
+    void isEmpty() {
+        assertFalse(st.isEmpty());
+        st.pop();
+        st.pop();
+        assertTrue(st.isEmpty());
+    }
+
+    @Test
+    void realStack() {
+        Stack<Integer> stack = new Stack<>();
+        assertThrows(EmptyStackException.class, () -> {
+            stack.pop();
+        });
+    }
+}
